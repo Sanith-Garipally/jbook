92e76ffbff995b5279e7762c88cc74e156321ce1
Sanith Garipally <sanith.garipally.19.1@mountblue.tech>
2025-03-30 16:15:10 +0530

    Add: Redux Store Setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,21 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import ReactDOM from 'react-dom/client';
 import CodeCell from './components/code-cell';
+import TextEditor from './components/text-editor';
+import { Provider } from 'react-redux';
+import { store } from './state';
 const el = document.getElementById('root');
 
 const root = ReactDOM.createRoot(el!);
 
 const App = () => {
   return (
-    <div>
-      <CodeCell />
-    </div>
+    <Provider store={store}>
+      <div>
+        {/* <CodeCell /> */}
+        <TextEditor />
+      </div>
+    </Provider>
   );
 };
 
